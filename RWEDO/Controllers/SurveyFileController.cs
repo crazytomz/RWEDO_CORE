887150e7918cf72992f4a804349d7198a9e57399
Thomson K Varkey <thomsonkvarkey@outlook.com>
2020-11-18 09:36:48 +0530

    Changes with controller and viewmodel

---
+++ RWEDO/Controllers/SurveyFileController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RWEDO.MSQLRepository.Contracts;
+
+namespace RWEDO.Controllers
+{
+    public class SurveyFileController : Controller
+    {
+        private readonly ISuveyFileRepository _surveyFileRepository;
+        public SurveyFileController(ISuveyFileRepository surveyFileRepository)
+        {
+            _surveyFileRepository = surveyFileRepository;
+        }
+        public ViewResult Index()
+        {
+            var model = _surveyFileRepository.GetAllSurveyFile();                            
+            return View(model);
+        }
+    }
+}
---
+++ RWEDO/ViewModels/SurveyFileViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RWEDO.ViewModels
+{
+    public class SurveyFileViewModel
+    {
+        public int ID { get; set; }
+        [Required]
+        public int Index { get; set; }
+        [Required]
+        public string Date { get; set; }
+        [Required]
+        public int InsurerID { get; set; }
+        public int RepairerID { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Insured { get; set; }
+        public string EstimateDate { get; set; }
+        public string BillDate { get; set; }
+        public string FollowUpDate { get; set; }
+    }
+}
